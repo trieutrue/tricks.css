a2ac5c4ad26cd28f8757151ccf30873fb63a37ce
JT <ttran2193@gmail.com>
2020-12-09 00:36:18 -0500

    Create blinker animation render background image

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import "./styles/reset.scss";
 import "./styles/nav.scss";
 import "./styles/animations.scss";
 import "./styles/index.scss";
+
+const text = "Tricks"
+
+const typewriter = (txt) => {
+  if (i < txt.length) {
+    document.getElementById("logo").innerHTML += txt[i]
+    i++;
+    setTimeout(typeWriter, 250)
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  typewriter(text)
+})
