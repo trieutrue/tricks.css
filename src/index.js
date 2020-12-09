0f54023dbf1970f99ca393c6e76dcfc399c3269f
JT <ttran2193@gmail.com>
2020-12-09 16:05:55 -0500

    debug handlePages

--- src/index.js
+++ src/index.js
@@ -23,20 +23,36 @@ const text = "Tricks"
 //Save ref to current section?
 //set current section height animation to 0%
 //set next section height animation to 100
-const handleHashLinks = e => {
+const resetSelectedAttribute = () => {
+  const btns = document.getElementsByClassName("circle-btn")
+  const pages = document.getElementsByClassName("scroll-page")
+
+  for (let i = 0; i < btns.length; i++) {
+    const btn = btns[i];
+    const page = pages[i];
+    if(Boolean(btn.attributes["selected"].value)) btn.attributes["selected"].value = ""
+    if(Boolean(page.attributes["selected"].value)) page.attributes["selected"].value = ""
+  }
+}
+
+
+const handlePages = e => {
   e.preventDefault();
   debugger
-  const { toggleAttribute, attributes } = e.target
-  if (Boolean(attributes["selected"].value)) return
+  const { selected, href } = e.currentTarget.attributes
+  if (Boolean(selected.value)) return
 
-  attributes["selected"] = true
-  document.getElementById(e.currentTarget.hash.slice(1))
-    .animate({ top: ["100%", 0] }, 600)
+  debugger
+  resetSelectedAttribute()
+  document.getElementById(href.value).attributes["selected"].value = true
+  selected.value = true
+  // document.getElementById(e.currentTarget.hash.slice(1))
+  //   .animate({ top: ["100%", 0] }, 600)
 }
 
 //Add event listener to each <a> tag
-const scrollLinks = document.getElementById("side-nav").querySelectorAll("a")
+const scrollLinks = document.getElementById("side-nav").querySelectorAll("div")
 for (let i = 0; i < scrollLinks.length; i++) {
   const element = scrollLinks[i];
-  element.addEventListener("click", handleHashLinks)
+  element.addEventListener("click", handlePages)
 }
